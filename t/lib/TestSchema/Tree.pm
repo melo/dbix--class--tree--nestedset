79cd2213df7fd8386fb34d3613c4e96e16af2233
rafl <rafl@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2008-07-25 00:43:05 +0000

    A simple test schema.
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/DBIx-Class-Tree-NestedSet@4622 bd8105ee-0ff8-0310-8827-fb3f25b6796d

---
+++ t/lib/TestSchema/Tree.pm
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+package TestSchema::Tree;
+
+use parent 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/Tree::NestedSet Core/);
+__PACKAGE__->table('tree');
+
+__PACKAGE__->tree_columns({
+    root_column  => 'root',
+    left_column  => 'lft',
+    right_column => 'rgt',
+});
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type         => 'integer',
+        is_auto_increment => 1,
+    },
+    root => {
+        data_type   => 'integer',
+        is_nullable => 1,
+    },
+    lft     => { data_type => 'integer' },
+    rgt     => { data_type => 'integer' },
+    content => { data_type => 'text'    },
+);
+
+__PACKAGE__->set_primary_key(qw/id/);
+
+1;
